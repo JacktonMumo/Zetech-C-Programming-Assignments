a07ae3d654e159f10a2aed003936b0a44751370d
JacktonMumo <pantheranard@gmail.com>
2024-09-08 22:46:46 +0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hello world");
+    printf("My name is Jackton Mumo");
+    printf("I am a computer scientist");
+    printf("I study at Zetech University");
+
+    return 0;
+}
