a58adb8b59c3ebb4d162ccd0259fc4db70f465bc
JacktonMumo <pantheranard@gmail.com>
2024-09-10 21:55:10 +0300

    Add files via upload

---
+++ area.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int length, width, area;
+    printf("Enter the length of the rectangle:");
+    scanf("%d",&length);
+    printf("Enter the width of the rectangle:");
+    scanf("%d",&width);
+    area = length * width;
+    printf("The sum of the two is %d",width);
+    return 0;
+}
